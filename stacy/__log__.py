40ca743d180bed49ace453c1236db5d25fd5588d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:31:18 -0200

    Saved stacy/__log__.py at Timestamp: 2018-11-14 13:31:18

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -81,3 +81,10 @@ IndentationError: unexpected indent
   ^
 IndentationError: unexpected indent
 '''},
+{'date': 'Wed Nov 14 2018 11:31:17.901 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 10
+  tit="Bob",
+  ^
+IndentationError: unexpected indent
+'''},
