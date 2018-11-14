e68da7e57db59d9f13cc4a8f71827fa27c379d67
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:07:11 -0200

    Created naomi/main.py at Timestamp: 2018-11-14 13:07:11

---
+++ naomi/main.py
@@ -0,0 +1,7 @@
+# adele.naomi.main.py
+#Mariana Reigada
+from _spy.vitollino.main import Texto, Elemento, Cena 
+PRAIA = "https://http2.mlstatic.com/mega-banner-painel-decoraco-festa-240x150-praia-D_NQ_NP_826579-MLB25851925736_082017-F.jpg"
+ESTRELA = "https://cdn.pixabay.com/photo/2017/06/25/10/44/isolated-2440384_960_720.png"
+
+def Historia():
