2a0780f6d7fab57c9b721b51a8eb48eda4f4b059
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:29:19 -0200

    Saved stacy/__log__.py at Timestamp: 2018-11-14 13:29:19

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -42,3 +42,21 @@ NameError: name 'elemento' is not defined
     elementoEsponja = elemento(img=PRAIA,
 NameError: name 'elemento' is not defined
 '''},
+{'date': 'Wed Nov 14 2018 11:29:18.161 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 14
+    Historia()
+  module <module> line 9
+    elementoEsponja = elemento(img=PRAIA,
+NameError: name 'elemento' is not defined
+'''},
