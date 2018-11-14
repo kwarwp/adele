d3fd739381574b1a10da4421adc47f50ce00846c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:16:18 -0200

    Created callie/main.py at Timestamp: 2018-11-14 13:16:17

---
+++ callie/main.py
@@ -0,0 +1,10 @@
+# adele.callie.main.py
+# Leticia Santos
+from _spy.vitollino.main import Texto, Elemento, Cena
+NOITEESTRELADA= "https://2.bp.blogspot.com/-Xd04iybAUfk/W4eX8xaX8fI/AAAAAAAAN-Y/2xJQ-GhwQKEkp9w8KIrUPNF_2zYRNLwIQCLcBGAs/s2560/milky-way-2560x1440-starry-sky-evening-panorama-landscape-5k-15249.jpg"
+JASMINE= "https://banner2.kisspng.com/20180713/zjx/kisspng-princess-jasmine-the-magic-carpets-of-aladdin-jafa-disney-aladdin-5b490dbee1d456.405061751531514302925.jpg"
+
+def Historia():
+    cenaNoiteestrelada = Cena(NOITEESTRELADA)
+    cenaNoiteestrelada.vai()
+    Historia()
