5b2370aef5694cdec659f6c1b490afa196544198
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:13:40 -0200

    Saved kathryn/main.py at Timestamp: 2018-11-14 13:13:39

--- kathryn/main.py
+++ kathryn/main.py
@@ -4,3 +4,7 @@ from _spy.vitollino.main import Texto, Elemento, Cena
 LONDRES = "https://mapadelondres.org/wp-content/uploads/2011/11/londres-tower-bridge-neve-istock.jpg"
 HANNA = "https://png2.kisspng.com/20180525/jfu/kisspng-ashley-benson-hanna-marin-pretty-little-liars-aria-5b0844e7477e04.1931477415272685832929.png
 "
+def Historia ():
+    cenaLondres = Cena (LONDRES)
+    cenaLondres.vai()
+Historia()
