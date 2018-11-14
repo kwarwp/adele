8b85d639607179d82c6924e968396050b890013f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:34:25 -0200

    Saved stacy/__log__.py at Timestamp: 2018-11-14 13:34:24

--- stacy/__log__.py
+++ stacy/__log__.py
@@ -137,3 +137,10 @@ IndentationError: unexpected indent
                                                                                                                                                                        ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 14 2018 11:34:24.43 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  elementoEsponja = Elemento(img=PRAIA,
+                                                                                                                                                                       ^
+SyntaxError: invalid syntax
+'''},
