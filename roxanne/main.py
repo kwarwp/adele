4b964dac1d7c93b9984673d85ffea28ba8c5342b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:02:07 -0200

    Created roxanne/main.py at Timestamp: 2018-11-14 13:02:07

---
+++ roxanne/main.py
@@ -0,0 +1,5 @@
+# adele.roxanne.main.py
+# Cibele Ribeiro
+from _spy.vitollino.main import Texto, Elemento, Cena
+ARGENTINA = "https://i0.wp.com/aquimequedo.com.br/wp-content/uploads/2018/06/Bar-Sur-P1020365.jpg?resize=1024%2C683"
+TANGO = "http://pluspng.com/img-png/ballroom-dancing-png-hd-learn-to-dance-salsa-cha-cha-bachata-bellydancing-hip-hop-1600.png"
