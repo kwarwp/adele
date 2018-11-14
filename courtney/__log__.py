1226dd4d3733db3268315904714c0b7a1080048c
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:31:32 -0200

    Saved courtney/__log__.py at Timestamp: 2018-11-14 13:31:31

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -6,3 +6,10 @@
                                                                                                                                                                  ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 14 2018 11:31:44.141 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  elementoFlamengo = (img=FLAMENGO, 
+                                                                                                                                                                 ^
+SyntaxError: invalid syntax
+'''},
