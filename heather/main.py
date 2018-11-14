206af5a0ea79a8f751759b16d48591836a78bf50
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:14:16 -0200

    Created heather/main.py at Timestamp: 2018-11-14 13:14:16

---
+++ heather/main.py
@@ -0,0 +1,12 @@
+# adele.heather.main.py
+# Hellen Wagmacker
+from _spy.vitollino.main import Texto, Elemento, Cena
+GUARDA_CHUVA = "https://www.clipartmax.com/png/middle/223-2232575_umbrella-yellow-rain-icon-yellow-umbrella-png.png"
+NOVA_YORK = "https://s3-media4.fl.yelpcdn.com/bphoto/Y7P32v34MaACfkvwUmO2xg/o.jpg"
+Heather = "https://vignette.wikia.nocookie.net/hijotee827/images/8/84/Nice_Heather.png/revision/latest?cb=20140609233018"
+
+def Historia():
+    cenanovayork = Cena(NOVA_YORK)
+    cenanovayork.vai()
+    
+Historia()
