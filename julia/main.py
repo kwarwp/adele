f209759d25d7bc3f8f2a72e01d8d6d87fc57746b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:17:33 -0200

    Saved julia/main.py at Timestamp: 2018-11-14 13:17:33

--- julia/main.py
+++ julia/main.py
@@ -1,6 +1,6 @@
 # adele.julia.main.py
 # Julia Pereira Soares 
-from _spy.vitollino.main import Texto, Elemento, cena 
+from _spy.vitollino.main import Texto, Elemento, Cena 
 CASTELO = "https://http2.mlstatic.com/fundo-fotografico-painel-lona-castelo-dentro-6x3-displays-D_NQ_NP_153815-MLB25311821030_012017-F.jpg"
 VALSA = "http://4.bp.blogspot.com/-leQ5oaWmMqw/Ua-S6mJJ4BI/AAAAAAAADTg/HhAtv_nrD1A/s640/bela_fera.gif"
 
