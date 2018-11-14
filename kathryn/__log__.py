a6baccb1607b7abaab41f522714854914a5f4d24
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:32:04 -0200

    Saved kathryn/__log__.py at Timestamp: 2018-11-14 13:32:04

--- kathryn/__log__.py
+++ kathryn/__log__.py
@@ -13,3 +13,10 @@ SyntaxError: invalid syntax
                                                                                                             ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 14 2018 11:32:14.484 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  elementoHanna = Elemento(img=HANNA,
+                                                                                                            ^
+SyntaxError: invalid syntax
+'''},
