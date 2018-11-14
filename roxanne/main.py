5158496b4bcdd12b8488e8fced4a73830f4a0e13
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:13:27 -0200

    Saved roxanne/main.py at Timestamp: 2018-11-14 13:13:27

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,3 +3,8 @@
 from _spy.vitollino.main import Texto, Elemento, Cena
 ARGENTINA = "https://i0.wp.com/aquimequedo.com.br/wp-content/uploads/2018/06/Bar-Sur-P1020365.jpg?resize=1024%2C683"
 TANGO = "http://pluspng.com/img-png/ballroom-dancing-png-hd-learn-to-dance-salsa-cha-cha-bachata-bellydancing-hip-hop-1600.png"
+
+def Historia():
+    cenaArgentina = Cena(ARGENTINA)
+    cenaArgentina.vai()
+Historia()
