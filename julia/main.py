10a4e21cc0fbe3bd57033c1293c539dedc724eb1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:17:00 -0200

    Saved julia/main.py at Timestamp: 2018-11-14 13:17:00

--- julia/main.py
+++ julia/main.py
@@ -4,7 +4,7 @@ from _spy.vitollino.main import Texto, Elemento, cena
 CASTELO = "https://http2.mlstatic.com/fundo-fotografico-painel-lona-castelo-dentro-6x3-displays-D_NQ_NP_153815-MLB25311821030_012017-F.jpg"
 VALSA = "http://4.bp.blogspot.com/-leQ5oaWmMqw/Ua-S6mJJ4BI/AAAAAAAADTg/HhAtv_nrD1A/s640/bela_fera.gif"
 
- def Historia():
+def Historia():
     cenaCastelo = Cena(CASTELO)
     cenaCastelo.vai()
 Historia()
