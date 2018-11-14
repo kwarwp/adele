ae54ae3f828aec009510ace8eb596f027661cd0e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:24:27 -0200

    Created stacy/__log__.py at Timestamp: 2018-11-14 13:24:27

---
+++ stacy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Nov 14 2018 11:24:24.750 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  elementoEsponja = elemento(img=PRAIA,
+                                                                                                                                        ^
+SyntaxError: invalid syntax
+'''},
