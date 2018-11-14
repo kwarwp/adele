62775fa08a3fb000bb34e4894a439e477a21f35d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:15:40 -0200

    Saved kathryn/main.py at Timestamp: 2018-11-14 13:15:40

--- kathryn/main.py
+++ kathryn/main.py
@@ -5,6 +5,6 @@ LONDRES = "https://mapadelondres.org/wp-content/uploads/2011/11/londres-tower-br
 HANNA = "https://png2.kisspng.com/20180525/jfu/kisspng-ashley-benson-hanna-marin-pretty-little-liars-aria-5b0844e7477e04.1931477415272685832929.png"
 
 def Historia ():
-    cenaLondres = Cena (LONDRES)
+    cenaLondres = Cena(LONDRES)
     cenaLondres.vai()
 Historia()
