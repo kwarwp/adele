f81da5a81e6ad7844e0fb53baf5ad267dc3c70ef
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-14 11:28:46 -0200

    Created courtney/__log__.py at Timestamp: 2018-11-14 13:28:46

---
+++ courtney/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Nov 14 2018 11:28:56.164 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 8
+  elementoMaracana = (img=MARACANA, 
+                                                                                                                                                                 ^
+SyntaxError: invalid syntax
+'''},
