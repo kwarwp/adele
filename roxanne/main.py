c1f410720f1f689ee80e58cd203a1081ab384bb1
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-21 12:04:23 -0200

    Saved roxanne/main.py at Timestamp: 2018-11-21 14:04:23

--- roxanne/main.py
+++ roxanne/main.py
@@ -3,12 +3,14 @@
 from _spy.vitollino.main import Texto, Elemento, Cena
 ARGENTINA = "https://i0.wp.com/aquimequedo.com.br/wp-content/uploads/2018/06/Bar-Sur-P1020365.jpg?resize=1024%2C683"
 TANGO = "https://i1.wp.com/www.donaheys.co.uk/wp-content/uploads/Strictly-Come-Dancing-Robin-Windsor-Kristina-Rihanoff-290px.gif?fit=187%2C300"
-
+SAMBA = "https://st2.depositphotos.com/8348572/11686/v/950/depositphotos_116863036-stock-illustration-brazilian-couple-performing-samba-dance.jpg"
 def Historia():
     cenaArgentina = Cena(ARGENTINA)
     elementoTango = Elemento(img=TANGO,
                             tit="Casal",
-                            style=dict(left=80, top=60, width=80, height=200))
+                            style=dict(left=180, top=30, width='120px', height='260px'))
     elementoTango.entra(cenaArgentina) 
+    cenaSamba = Cena(SAMBA, direita=cenaArgentina)
+    cenaArgentina.esquerda = cenaSamba
     cenaArgentina.vai()
 Historia()
