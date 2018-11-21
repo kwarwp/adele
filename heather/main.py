5302a02788e3d6e8705a5fcb1567a9e82f0a1120
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-21 11:20:38 -0200

    Saved heather/main.py at Timestamp: 2018-11-21 13:20:38

--- heather/main.py
+++ heather/main.py
@@ -1,7 +1,7 @@
 # adele.heather.main.py
 # Hellen Wagmacker
 from _spy.vitollino.main import Texto, Elemento, Cena
-GUARDA_CHUVA = "https://www.clipartmax.com/png/middle/223-2232575_umbrella-yellow-rain-icon-yellow-umbrella-png.png"
+GUARDA_CHUVA = "https://banner2.kisspng.com/20180301/lkq/kisspng-yellow-umbrella-angle-umbrella-5a980c04733553.3837393615199139884719.jpg"
 NOVA_YORK = "https://s3-media4.fl.yelpcdn.com/bphoto/Y7P32v34MaACfkvwUmO2xg/o.jpg"
 Heather = "https://vignette.wikia.nocookie.net/hijotee827/images/8/84/Nice_Heather.png/revision/latest?cb=20140609233018"
 Barney = "https://icon2.kisspng.com/20180530/ipc/kisspng-neil-patrick-harris-barney-stinson-how-i-met-your-barney-stinson-5b0e55230325a2.0860035615276659550129.jpg"
@@ -10,8 +10,8 @@ Abacaxi = "https://banner2.kisspng.com/20171127/382/pineapple-png-vector-clipart
 
 def Historia():
     cenanovayork = Cena(NOVA_YORK)
-    elementoGuardaChuva = Elemento(img=GUARDA_CHUVA,
-                                  tit="Guarda Chuva",
+    elementoGuardaChuva = Elemento(img=Cabra,
+                                  tit="Cabra",
                                   style=dict(left=150, top=60, width=60, height=200))
     cenanovayork.vai()
     elementoGuardaChuva.entra(cenanovayork)
