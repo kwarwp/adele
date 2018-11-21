ba772f74d8bd37457693d4089512adbdc84e4d98
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-21 11:43:14 -0200

    Saved roxanne/__log__.py at Timestamp: 2018-11-21 13:43:14

--- roxanne/__log__.py
+++ roxanne/__log__.py
@@ -6,3 +6,25 @@
                                         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Wed Nov 21 2018 11:43:26.114 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 16
+    Historia()
+  module <module> line 13
+    cenaSamba = Cena(SAMBA, dir=cenaArgentina)
+  module _spy.vitollino.main line 778
+    Cena.c(**kwargs)
+  module _spy.vitollino.main line 821
+    imagem, kwargs = (imagem, {}) if isinstance(imagem, str) \
+AttributeError: 'Cena' object has no attribute '__getitem__'
+'''},
