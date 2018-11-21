31bb3fc5638a235978a4251f8e408609caca9d14
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-21 11:42:46 -0200

    Created roxanne/__log__.py at Timestamp: 2018-11-21 13:42:45

---
+++ roxanne/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Wed Nov 21 2018 11:42:56.370 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 13
+  cenaSamba = (SAMBA, dir=cenaArgentina)
+                                        ^
+SyntaxError: invalid syntax
+'''},
