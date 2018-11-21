54555b3a4b30c28eabf0bd995bf1d9ba70dee7a0
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-21 11:14:15 -0200

    Saved naomi/main.py at Timestamp: 2018-11-21 13:14:14

--- naomi/main.py
+++ naomi/main.py
@@ -2,7 +2,7 @@
 #Mariana Reigada
 from _spy.vitollino.main import Texto, Elemento, Cena 
 PRAIA = "https://http2.mlstatic.com/mega-banner-painel-decoraco-festa-240x150-praia-D_NQ_NP_826579-MLB25851925736_082017-F.jpg"
-ESTRELA = "https://cdn.pixabay.com/photo/2017/06/25/10/44/isolated-2440384_960_720.png"
+ESTRELA = "https://www.aixalonjadeartesania.pt/_articulos/estrella-de-mar-espina-15-18-cm--0002289.png"
 
 def Historia():
     cenaPraia = Cena(PRAIA)
