821c271e4511eb3c8fb136e008f7e9f71950ac9d
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-21 12:03:10 -0200

    Saved heather/main.py at Timestamp: 2018-11-21 14:03:10

--- heather/main.py
+++ heather/main.py
@@ -1,18 +1,21 @@
 # adele.heather.main.py
 # Hellen Wagmacker
 from _spy.vitollino.main import Texto, Elemento, Cena
-GUARDA_CHUVA = "https://banner2.kisspng.com/20180301/lkq/kisspng-yellow-umbrella-angle-umbrella-5a980c04733553.3837393615199139884719.jpg"
 NOVA_YORK = "https://s3-media4.fl.yelpcdn.com/bphoto/Y7P32v34MaACfkvwUmO2xg/o.jpg"
 Heather = "https://vignette.wikia.nocookie.net/hijotee827/images/8/84/Nice_Heather.png/revision/latest?cb=20140609233018"
-Barney = "https://icon2.kisspng.com/20180530/ipc/kisspng-neil-patrick-harris-barney-stinson-how-i-met-your-barney-stinson-5b0e55230325a2.0860035615276659550129.jpg"
-Cabra = "https://banner2.kisspng.com/20171220/qte/goat-png-5a3af391b0bd49.43952778151381288172398118.jpg"
-Abacaxi = "https://banner2.kisspng.com/20171127/382/pineapple-png-vector-clipart-image-5a1c46718ae5b2.5079999715118024815689.jpg"
-Cabra1 = "https://banner2.kisspng.com/20180909/vop/kisspng-goat-milk-goat-milk-cattle-sheep-png-5b95d3ba0db405.8161898815365457220561.jpg"
+Barney = "https://c7.uihere.com/files/30/808/836/neil-patrick-harris-choose-your-own-autobiography-how-i-met-your-mother-barney-stinson-desi-collings-how-i-met-your-mother.jpg"
 def Historia():
     cenanovayork = Cena(NOVA_YORK)
-    elementoGuardaChuva = Elemento(img= Cabra,
-                                  tit="Cabra",
+    elementoGuardaChuva = Elemento(img= Heather,
+                                  tit="Heather",
                                   style=dict(left=150, width=60, height=100))
+
+    cenanovayork = Cena(NOVA_YORK)
+    elementoBarney = Elemento(img= Barney,
+                                  tit="Barney",
+                                  style=dict(left=150, width=60, height=100))
+
     cenanovayork.vai()
+    elementoBarney.entra(cenanovayork)
     elementoGuardaChuva.entra(cenanovayork)
 Historia()
