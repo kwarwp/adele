eae906fe4de7d892c854f275d573e823969156ec
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-28 11:31:16 -0200

    Saved roxanne/main.py at Timestamp: 2018-11-28 13:31:16

--- roxanne/main.py
+++ roxanne/main.py
@@ -1,16 +1,19 @@
 # adele.roxanne.main.py
 # Cibele Ribeiro
-from _spy.vitollino.main import Texto, Elemento, Cena
+from _spy.vitollino.main import Texto, Elemento, Cena, INVENTARIO
 ARGENTINA = "https://i0.wp.com/aquimequedo.com.br/wp-content/uploads/2018/06/Bar-Sur-P1020365.jpg?resize=1024%2C683"
 TANGO = "https://i1.wp.com/www.donaheys.co.uk/wp-content/uploads/Strictly-Come-Dancing-Robin-Windsor-Kristina-Rihanoff-290px.gif?fit=187%2C300"
 SAMBA = "https://st2.depositphotos.com/8348572/11686/v/950/depositphotos_116863036-stock-illustration-brazilian-couple-performing-samba-dance.jpg"
 def Historia():
+    INVENTARIO.inicia()
     cenaArgentina = Cena(ARGENTINA)
     elementoTango = Elemento(img=TANGO,
                             tit="Casal",
                             style=dict(left=180, top=30, width='120px', height='260px'))
     elementoTango.entra(cenaArgentina) 
-    txttango = Texto(cenaArgentina,"Quem dança seus males espanta!")
+    def botainventario():    
+        INVENTARIO.bota(elementoTango)
+    txttango = Texto(cenaArgentina,"Quem dança seus males espanta!", foi=botainventario)
     elementoTango.vai = txttango.vai    
     cenaSamba = Cena(SAMBA, direita=cenaArgentina)
     cenaArgentina.esquerda = cenaSamba
