2c4c96a1b9535b530844dbba1e809eaadc87c35e
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-28 10:45:03 -0200

    Saved courtney/__log__.py at Timestamp: 2018-11-28 12:45:02

--- courtney/__log__.py
+++ courtney/__log__.py
@@ -79,3 +79,21 @@ NameError: name 'cenaMaracana' is not defined
     txtflamengo = Texto(cenaMaracana, "Vamos Flamengo")
 NameError: name 'cenaMaracana' is not defined
 '''},
+{'date': 'Wed Nov 28 2018 10:44:58.406 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 19
+    Historia()
+  module <module> line 15
+    cenaBAR = Cena(BAR, direita=CenaMaracana)
+NameError: name 'CenaMaracana' is not defined
+'''},
