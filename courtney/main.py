641b5a2ed4e73bd6f778a4eb9654f43ee1b24377
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-28 10:44:32 -0200

    Saved courtney/main.py at Timestamp: 2018-11-28 12:44:32

--- courtney/main.py
+++ courtney/main.py
@@ -3,6 +3,7 @@
 from _spy.vitollino.main import Texto, Elemento, Cena 
 MARACANA="https://i.ytimg.com/vi/nY-BUXppCSU/maxresdefault.jpg" 
 FLAMENGO= "https://fla-bucket-s3-us.s3.amazonaws.com/public/images/players/2/1524238563.png" 
+BAR= "https://augustonobuteco.files.wordpress.com/2011/11/dsc05501reduzida1.jpg"
 def Historia():
     cenaMaracana = Cena(MARACANA)
     elementoFlamengo = Elemento(img=FLAMENGO, 
@@ -11,5 +12,8 @@ def Historia():
     elementoFlamengo.entra(cenaMaracana)
     txtflamengo = Texto(cenaMaracana, "Vamos Flamengo")
     elementoFlamengo.vai =txtflamengo.vai
+    cenaBAR = Cena(BAR, direita=CenaMaracana)
+    cenaMaracana.esquerda = cenaBAR
+    
     cenaMaracana.vai()
 Historia()
