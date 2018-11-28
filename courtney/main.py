305cdfd069d774b14df8420aaf748740ecef3686
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-11-28 11:07:06 -0200

    Saved courtney/main.py at Timestamp: 2018-11-28 13:07:06

--- courtney/main.py
+++ courtney/main.py
@@ -5,13 +5,14 @@ MARACANA="https://i.ytimg.com/vi/nY-BUXppCSU/maxresdefault.jpg"
 FLAMENGO= "https://fla-bucket-s3-us.s3.amazonaws.com/public/images/players/2/1524238563.png" 
 BAR= "https://augustonobuteco.files.wordpress.com/2011/11/dsc05501reduzida1.jpg"
 def Historia():
+    INVENTARIO.inicia()
     cenaMaracana = Cena(MARACANA)
     elementoFlamengo = Elemento(img=FLAMENGO, 
                        tit="Torcida do Flamengo",
                        style=dict(left=150, width=60, heigth=70))
     elementoFlamengo.entra(cenaMaracana)
     def botainventario():
-    Inventario.bota(elementoFlamengo)
+    INVENTARIO.bota(elementoFlamengo)
     txtflamengo = Texto(cenaMaracana, "Vamos Flamengo")
     elementoFlamengo.vai =txtflamengo.vai
     cenaBAR = Cena(BAR, direita=cenaMaracana)
