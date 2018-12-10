cd740c8da2b0e352a0983b9003b0a2ce808dc479
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-10 11:54:50 -0200

    Saved tracy/main.py at Timestamp: 2018-12-10 13:54:50

--- tracy/main.py
+++ tracy/main.py
@@ -2,6 +2,13 @@
 #Mariana Reigada
 from _spy.vitollino.main import Texto, Elemento, Cena 
 BABY = "https://i.imgur.com/ymsBTXi.png"
+def Historia():
+    BABY = Cena(BABY)
+    
+    cenaBABY.vai = cena(UFRJ, direita=cenaBABY)
+    
+Historia()    
+                                      
 
 
    
