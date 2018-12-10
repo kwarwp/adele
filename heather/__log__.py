dd3aeaa67ce5d27c0f640e03b126607bdced7cff
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-10 11:54:20 -0200

    Saved heather/__log__.py at Timestamp: 2018-12-10 13:54:20

--- heather/__log__.py
+++ heather/__log__.py
@@ -74,3 +74,21 @@ IndentationError: unexpected indent
     cenaUFRJ = cena(UFRJ, direita=cenaUFRJ)
 NameError: name 'cena' is not defined
 '''},
+{'date': 'Mon Dec 10 2018 11:53:11.0 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 21
+    Historia()    
+  module <module> line 18
+    cenaUFRJ = cena(UFRJ, direita=cenaUFRJ)
+NameError: name 'cena' is not defined
+'''},
