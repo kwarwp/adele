aa720744b5aa2b6adb6e149bfdd7c402e3c4c52b
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-10 11:50:29 -0200

    Created tracy/__log__.py at Timestamp: 2018-12-10 13:50:28

---
+++ tracy/__log__.py
@@ -0,0 +1,8 @@
+
+{'date': 'Mon Dec 10 2018 11:49:18.499 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 9
+  Historia()    
+  ^
+IndentationError: expected an indented block
+'''},
