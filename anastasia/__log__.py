e109b97b943457ab112059f058b12549737ab071
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-13 11:20:01 -0300

    Saved anastasia/__log__.py at Timestamp: 2019-05-13 14:20:00

--- anastasia/__log__.py
+++ anastasia/__log__.py
@@ -6,3 +6,10 @@
         ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Mon May 13 2019 11:19:59.614 GMt-0300 (Horário Padrão de Brasília) -X- SuPyGirls -X-',
+'error': '''
+ module <string> line 4
+  Bart Simpson = "http://www.stickpng.com/assets/thumbs/5a0c40785a997e1c2cea1168.png"
+        ^
+SyntaxError: invalid syntax
+'''},
