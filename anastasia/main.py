bca17f9cdedd44c48cc537fd6e875bd06897795a
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-05-13 11:30:05 -0300

    Created anastasia/main.py at Timestamp: 2019-05-13 14:30:04

---
+++ anastasia/main.py
@@ -0,0 +1,20 @@
+# adele.anastasia.main.py
+from _spy.vitollino.main import Cena , Texto , Elementos
+from _spy.vitollino.main import inventario as inv
+Bart.Simpson = "https://www.multarte.com.br/wp-content/uploads/2019/03/bart-simpson-hypebeast-3.png"
+Mar = "https://r-ec.bstatic.com/images/hotel/max1024x768/650/65026569.jpg"
+Furia.da.Noite = "https://i.pinimg.com/originals/1e/45/c7/1e45c7fd135d3b8fed8f18a33447a019.png"
+Vulcao = "https://mundoeducacao.bol.uol.com.br/upload/conteudo_legenda/aed822dbf3f1a9eaf83218845b978bf7.jpg"
+def oi ():
+Mar = cena(img = Mar)
+Vulcao = cena (img = Vulcao)
+Mar.esquerda = Vulcao
+Vulcao.direita = Mar
+Bart Simpson = Elemento (img = Bart Simpson, tit"Oi, eu sou o Bart!", Style = dict(left=150, top=60, weith=60, heith=200))
+Bart Simpson.entra(mar)
+FalaBart=Texto(Mar,"E aí, estamos nas Ilhas Maldivas! Nem sei como consegui parar aqui.")
+Furia da Noite = Elemento (img = Furia da Noite, tit"Olá, sou o Furia da Noite", Style = dict(left=150, top=60, weith=60, heith=200))
+Furia da Noite.entra(vulcao)
+FalaFuria=Texto(Vulcao,"Estou animado pra poder sobrevoar e ver o que acontece lá dentro!")
+Mar.vai()
+Oi()
